621daa5c508aa4ee15cae006f73c537984ee410c
Mohammed Younus <mohammedyounus236@gmail.com>
2020-08-13 01:51:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASSS ='circle';
+const WINNING_COMBINATIONS =[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const restartButton = document.getElementById('restartButton');
+const winningMessageElement =document.getElementById('winningMessage');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+let circleTurn;
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame(){
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(CIRCLE_CLASSS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, {once: true})
+    })
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+}
+
+function handleClick(e){
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASSS : X_CLASS;
+    placeMark(cell, currentClass);
+
+    //Check for Win
+    if(checkWin(currentClass)){
+        endGame(false);
+    }
+    //Check for Draw
+    else if (isDraw()){
+        endGame(true);
+    }
+    //Switch Turns
+    else{
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function endGame(draw){
+    if(draw){
+        winningMessageTextElement.innerText = 'Draw!';
+    }
+    else{
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+    }
+
+    winningMessageElement.classList.add('show');
+
+}
+
+function isDraw(){
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASSS);
+    })
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass);
+}
+
+function swapTurns(){
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASSS);
+    if(circleTurn){
+        board.classList.add(CIRCLE_CLASSS);
+    }
+    else{
+        board.classList.add(X_CLASS);
+    }
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index =>{
+            return cellElements[index].classList.contains(currentClass);
+        })
+    })
+}
